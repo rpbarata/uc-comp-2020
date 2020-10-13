8f441251dfd7084735452433862114167279b09a
Rui Pedro Barata <ruipbarata@gmail.com>
2020-10-13 15:10:03 +0000

    Delete run_test.sh

--- run_test.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-file=$1
-if [ -f "Comp2020/meta1/${file}.uc" ]; then
-  ./uccompiler -l < "Comp2020/meta1/${file}.uc" | diff "Comp2020/meta1/${file}.out" -  
-else
-  ./uccompiler -l < "Comp2020/meta1/${file}.c" | diff "Comp2020/meta1/${file}.out" - 
-fi
